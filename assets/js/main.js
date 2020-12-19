2d755863784876a69ed6d8eec98525ea810f7076
blessing elendu <blesil86@gmail.com>
2020-12-19 15:54:23 +0100

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,4 @@
-// ----  Start Preloder
-/*const spinner = document.querySelector('.preloader');
-        let rotateCount = 0;
-        let startTime = null;
-        let rAF;
-        
-        let spinning = true;
 
-        function draw(timestamp) {
-            if (!startTime) {
-           startTime = timestamp;
-          }
-        
-          rotateCount = (timestamp - startTime) / 3;
-        
-          if (rotateCount > 359) {
-          rotateCount %= 360;
-        }
-        //to actually rotate the spinner
-        spinner.style.transform = `rotate(${rotateCount}deg)`;
-        
-        rAF = requestAnimationFrame(draw);
-        
-        };
-        draw()
-const html = document.querySelector('html');
-const preloaderArea = document.querySelector('.preloader-area');
-window.addEventListener('load', function(){
-    cancelAnimationFrame(rAF)
-        spinning = false;
-        preloaderArea.style.display = 'none';
-})*/
 
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
@@ -39,8 +8,8 @@ navBarToggle.addEventListener('click', function () {
     x.classList.toggle("change");
 });
 
-let navLinks = document.querySelector('.nav-links');
+/*let navLinks = document.querySelector('.nav-links');
 navLinks.addEventListener('click', function(){
     mainNav.classList.toggle('hide')
-})
+})*/
 
